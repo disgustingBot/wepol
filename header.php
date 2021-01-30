c1f7e91bc404d67679f35be2161cc6a89f827a88
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2021-01-30 13:11:20 +0200

    beggining of dark theme
    y otras hierbas

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ $scrt = '6LcRuNAUAAAAALBu7Ymh0yxmTXTJmP0rsnkjGyj0';
   <!-- <link rel="preconnect" href="https://fonts.gstatic.com"> -->
   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
-<body <?php body_class('Obse'); ?> data-observe=".body_activator" data-obse-area="-49% 0px -50% 0px">
+<body <?php body_class(); ?>>
 
 	<view id="load" class="load"></view>
 
@@ -23,42 +23,42 @@ $scrt = '6LcRuNAUAAAAALBu7Ymh0yxmTXTJmP0rsnkjGyj0';
   </div>
   <!-- <div class="redDot body_activator" data-clase="header_big"></div> -->
   <!-- <div class="redDot body_activator" data-clase="header_deployed"></div> -->
-  <header class="header Obse" id="header" data-observe=".header_activator">
-      <a class="logo" href="<?php echo site_url(); ?>">
-        <?php include 'assets/logo_horizontal.svg' ?>
-      </a>
-      <menu class="nav_bar_menu">
-        <?php
-        $args = array(
-          'theme_location' => 'nav_bar',
-          'depth' => 0,
-          'container'	=> false,
-          'fallback_cb' => false,
-          'menu_class' => 'nav_bar',
-        );
-        wp_nav_menu($args); ?>
-        <nav class="nav_bar_cat onlyMobileG">
+  <header class="header">
+    <a class="logo" href="<?php echo site_url(); ?>">
+      <?php include 'assets/logo_horizontal.svg' ?>
+    </a>
+    <menu class="nav_bar_menu">
+      <?php
+      $args = array(
+        'theme_location' => 'nav_bar',
+        'depth' => 0,
+        'container'	=> false,
+        'fallback_cb' => false,
+        'menu_class' => 'nav_bar',
+      );
+      wp_nav_menu($args); ?>
+      <nav class="nav_bar_cat onlyMobileG">
 
-        <?php
-        $categories = get_categories( array(
-            'orderby' => 'name',
-            'order'   => 'ASC'
-        ));
-        foreach( $categories as $category ) { ?>
-          <p class="nav_categories_item">
-            <a href="<?php echo get_term_link($category->term_id); ?>">
-              <?php echo $category->name; ?>
-            </a>
-          </p>
-        <?php } ?>
-        </nav>
-      </menu>
+      <?php
+      $categories = get_categories( array(
+          'orderby' => 'name',
+          'order'   => 'ASC'
+      ));
+      foreach( $categories as $category ) { ?>
+        <p class="nav_categories_item">
+          <a href="<?php echo get_term_link($category->term_id); ?>">
+            <?php echo $category->name; ?>
+          </a>
+        </p>
+      <?php } ?>
+      </nav>
+    </menu>
 
-      <button class="burger" onclick="altClassFromSelector('mobile_menu_active', '#header')">
-        <div class="burgerBar"></div>
-        <div class="burgerBar"></div>
-        <div class="burgerBar"></div>
-      </button>
+    <button class="burger" onclick="altClassFromSelector('mobile_menu_active', '.header')">
+      <div class="burgerBar"></div>
+      <div class="burgerBar"></div>
+      <div class="burgerBar"></div>
+    </button>
   </header>
 
   <nav class="nav_categories Carousel onlyDesktopG">
@@ -66,27 +66,19 @@ $scrt = '6LcRuNAUAAAAALBu7Ymh0yxmTXTJmP0rsnkjGyj0';
     <?php
     $i = 0;
     foreach( $categories as $category ) {
-      // algoritmo:
-      // empezar abriendo un element
-      // cada iteracion chequear el resto de $i / 4,
-      if ( $i != 0 and $i % 3 == 0) {
-        // si el resto es cero, cerrar el element y abrir otro
-        echo '</div> <div class="nav_categories_container rowcol1 Element">';
-      }
-      ?>
-        <p class="nav_categories_item">
-          <a href="<?php echo get_term_link($category->term_id); ?>">
-            <?php echo $category->name; ?>
-          </a>
-        </p>
-      <?php
-      $i += 1;
-       ?>
-    <?php } ?>
-  </div>
-  <p class="menu-item prenex prenex_next nextButton more_btn">
-    <span>Más categorías</span>
-    <?php echo standard_svg('arrow_right', 'more_btn_svg'); ?>
-  </p>
-
+      if ( $i != 0 and $i % 3 == 0) { ?>
+        </div>
+        <div class="nav_categories_container rowcol1 Element">
+      <?php } ?>
+      <p class="nav_categories_item">
+        <a href="<?php echo get_term_link($category->term_id); ?>">
+          <?php echo $category->name; ?>
+        </a>
+      </p>
+    <?php $i += 1; } ?>
+    </div>
+    <p class="menu-item prenex prenex_next nextButton more_btn">
+      <span>Más categorías</span>
+      <?php echo standard_svg('arrow_right', 'more_btn_svg'); ?>
+    </p>
   </nav>
