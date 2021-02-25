e1d37893d30066b8011803bde6e16b06d784827f
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2021-02-25 01:06:47 +0100

    Summary

--- inc/social_sharing.php
+++ inc/social_sharing.php
@@ -1,12 +1,12 @@
 <div class="social_sharing_container">
   <div class="dialogue_pointer"></div>
-  <a href="">
+  <a href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank">
     <?php echo standard_svg('facebook_svg', 'social_sharing_svg'); ?>
   </a>
-  <a href="">
+  <a href="mailto:?subject=I Mira este artículo&amp;body=<?php echo get_permalink(); ?>">
     <?php echo standard_svg('email_svg', 'social_sharing_svg'); ?>
   </a>
-  <a href="">
+  <a href="https://twitter.com/share?url=<?php echo get_permalink(); ?>&amp;text=Mira%este%artículo&amp;hashtags=encuestapuntocom" target="_blank">
     <?php echo standard_svg('twitter_svg', 'social_sharing_svg'); ?>
   </a>
   <a href="">
